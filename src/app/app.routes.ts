85d8334d70b63cff65ced4120f2796af44861e46
martinabertran1 <martinabertran65a@gmail.com>
2025-11-04 21:58:11 -0300

    home-admin component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes, provideRouter } from '@angular/router';
 import { Login } from './login/login';
 import { HomeUsuariosComponent } from './home-usuarios/home-usuarios.component';
-import { HomeAdmin } from './home-admin/home-admin';
+import { HomeAdminComponent } from './home-admin/home-admin.component';
 import { AuthGuard } from './services/auth-guard';
 import { ProductoFormComponent } from './pages/producto-form/producto-form.component';
 import { ListaProductosComponent } from './pages/lista-productos/lista-productos.component';
@@ -11,7 +11,7 @@ import { HistorialPedidoComponent } from './historial-pedido/historial-pedido';
 export const routes: Routes = [
     { path: '', component: HomeUsuariosComponent },
     { path: 'ingresar', component: Login },
-    { path: 'admin', component: HomeAdmin, canActivate: [AuthGuard] },
+    { path: 'admin', component: HomeAdminComponent, canActivate: [AuthGuard] },
     { path: 'productos', component: ProductoFormComponent, canActivate: [AuthGuard] },
     { path: 'lista-productos', component: ListaProductosComponent, canActivate: [AuthGuard] },
     { path: 'categorias', component: CategoriaFormComponent, canActivate: [AuthGuard] },
